9ae83bd519fb35750cb035c9e348a75e22850f1c
ye <93862812+yungsasha777@users.noreply.github.com>
2021-11-29 16:47:55 +0200

    enhanced a few lines

--- getjson.js
+++ getjson.js
@@ -1,8 +1,13 @@
 var xhttp = new XMLHttpRequest();
+let result = 'empty';
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    console.log(this.responseText);
-  }
+    fetchJson(JSON.parse(this.responseText));
+  };
 };
 xhttp.open("GET", "https://raw.githubusercontent.com/yungsasha777/21.11.21/main/db.json", true);
 xhttp.send();
+function fetchJson(arg){
+    result = arg;
+};
+console.log(result);
